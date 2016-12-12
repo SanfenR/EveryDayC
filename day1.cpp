574ff4754c6b2d339fc157c15ee76affece8bbcc
sanfen <sanfen@sanfendeMacBook-Pro.local>
2016-12-12 14:11:55 +0800

    day2

--- day1.cpp
+++ day1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+
 using namespace std;
 
 //
@@ -10,36 +11,40 @@ using namespace std;
 //题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字以及条件判断语句（A?B:C）。
 
 //----->fun1 start
-//使用函数指针
+//使用函数指针 递归加法
 
 typedef int (*fun)(int);
-int func1(int n){
+
+int func1(int n) {
     return 0;
 }
-int func2(int n){
-    fun f[2]={func1,func2};
-    return n+f[(n != 0)](n - 1);
+
+int func2(int n) {
+    fun f[2] = {func1, func2};
+    return n + f[(n != 0)](n - 1);
 }
 
-void function1(){
-    cout<<func2(10) << endl;
+void function1() {
+    cout << func2(10) << endl;
 }
 //----->fun2 end
 
 
 //----->fun2 start
 //使用静态变量
-class test{
+class test {
     static int N;
     static int sum;
 public :
-    test(){
+    test() {
         sum += ++N;
     }
-    static void reset(){
+
+    static void reset() {
         N = sum = 0;
     }
-    static int getSum(){
+
+    static int getSum() {
         return sum;
     }
 };
@@ -47,11 +52,11 @@ public :
 int test::N = 0;
 int test::sum = 0;
 
-void function2(){
+void function2() {
     test::reset();
-    test *p=new test[10];
-    cout<<test::getSum() << endl;
-    delete []p;
+    test *p = new test[10];
+    cout << test::getSum() << endl;
+    delete[]p;
 }
 //--->fun2 end
 
@@ -59,28 +64,30 @@ void function2(){
 //----->fun3 start
 //使用虚函数的编译多态性
 
-class A{
+class A {
 public:
-    virtual int sum(int n){return 0;};
-};
-class B:public A
-{
-public:
-    int sum(int n){
-        A a;B b;
-        A *p[2]={&a,&b};
-        return n+p[(n - 1 != 0)]->sum(n - 1);}
+    virtual int sum(int n) { return 0; };
 };
 
-void function3(){
+class B : public A {
+public:
+    int sum(int n) {
+        A a;
+        B b;
+        A *p[2] = {&a, &b};
+        return n + p[(n - 1 != 0)]->sum(n - 1);
+    }
+};
+
+void function3() {
     B b;
-    cout<<b.sum(10) << endl;
+    cout << b.sum(10) << endl;
 }
 
 //----->fun3 end
 
 
-int main(){
+int main() {
     function1();
     function2();
     function3();
---
+++ day2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+//
+// Created by sanfen on 16/12/12.
+//
+
+//题目: 求整数的2进制表示中1的个数
+
+
+//整数 右移 相与
+int function1(int i) {
+    int count = 0;
+    while(i) {
+        if(i & 1)
+            count ++;
+        i = i >> 1;
+    }
+    return count;
+}
+
+
+//flag 左移 相与
+int function2(int i) {
+    int count = 0;
+    unsigned int flag = 1;
+    while(flag){
+        if(i & flag){
+            count ++;
+        }
+        flag = flag << 1;
+    }
+    return count;
+}
+
+int function3(int i) {
+    int count = 0;
+    while (i) {
+        ++ count;
+        i = (i - 1) & i;
+    }
+    return count;
+}
+
+int function4(unsigned int x)
+{
+    int n=0;
+    while((x+1))
+    {
+        n++;
+        x=x|(x+1);
+    }
+    return n;
+}
+
+int main() {
+    cout << function1(6) << endl;
+    cout << function2(6) << endl;
+    cout << function3(6) << endl;
+    cout << function4(6) << endl;
+    return 0;
+}
