7f584954f0fe7a764627ee386c17aa550c34d19b
sanfenr <245021586@qq.com>
2017-01-10 11:18:01 +0800

    添加正数序列的一种特殊情况

--- day9.cpp
+++ day9.cpp
@@ -11,6 +11,7 @@
 // (2).sum==Sum。则输出序列，且将right后移
 // (3)对于固定的right.,sum>Sum时，始终将left左移动
 
+#include <list>
 #include"iostream"
 
 using namespace std;
@@ -48,24 +49,49 @@ void print(const int &left, const int &right) {
  * 简单写法
  * @param Sum
  */
-void findSequence(const int& Sum) {
-    int left =1, right = 1, sum = 1;
-    while(left <= Sum/2) {
+void findSequence(const int &Sum) {
+    int left = 1, right = 1, sum = 1;
+    while (left <= Sum / 2) {
         while (sum > Sum) {
             sum -= left;
-            left ++;
+            left++;
         }
         if (sum == Sum) {
             print(left, right);
         }
 
-        right ++;
+        right++;
         sum += right;
     }
+}
+
+// 题目：输入两个整数n和m，从数列1,2,3...n中随意取几个数，使其和等于m，要求列出所有的组合。
+
+
+list<int> list1;
+
+void find_factor(int sum, int n) {
+    //递归出口
+    if (n <= 0 || sum < 0)
+        return;
+    //输出找到的数
+    if (sum == n) {
+        list1.reverse();
+        for (list<int>::iterator iter = list1.begin(); iter != list1.end(); iter++)
+            cout << *iter << " + ";
+        cout << n << endl;
+        list1.reverse();
+    }
+    list1.push_front(n);
+    find_factor(sum - n, n - 1);//n参与查找
+    list1.pop_front();
 }
 
 
 int main() {
     findSequence(15);
+
+    find_factor(15, 5);
+
     return 0;
 }
