d3a949deaf7586c8d820d83a89828586d6e17c7f
sanfenr <245021586@qq.com>
2017-12-04 22:19:05 +0800

    一些修改

---
+++ HDU1004.cpp
@@ -0,0 +1,25 @@
+//
+// Created by sanfen on 2017/12/2.
+//
+//http://acm.hdu.edu.cn/showproblem.php?pid=1004
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    while(cin >> n) {
+        string *ss = new string[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> ss[i];
+        }
+        
+        ;
+    }
+
+    return 0;
+}
