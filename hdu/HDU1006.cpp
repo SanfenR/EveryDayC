5c0cd0b0aeb2356c20d139b8272cd8b1dac84ae0
sanfen <sanfenruxi1@163.com>
2017-12-06 21:06:01 +0800

    创建hud

---
+++ hdu/HDU1006.cpp
@@ -0,0 +1,14 @@
+//
+// Created by sanfen on 2017/12/6.
+//
+
+//http://acm.hdu.edu.cn/showproblem.php?pid=1006
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
